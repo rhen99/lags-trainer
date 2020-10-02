33e4e80101d81c4223cf7220148b84461e5d96d6
Arhen Santiago <lincoldarhen9900@gmail.com>
2020-10-02 18:08:37 +0800

    navigation made

--- client/src/components/layouts/Header.vue
+++ client/src/components/layouts/Header.vue
@@ -4,19 +4,32 @@
       <h1 class="header-title">
         LAGS Trainer <a href="#" class="hb-btn"><i class="fas fa-bars"></i></a>
       </h1>
+      <ul class="navigation">
+        <li class="navigation-item">
+          <a href="#">Register</a>
+        </li>
+        <li class="navigation-item">
+          <a href="#">Login</a>
+        </li>
+        <li class="navigation-item">
+          <a href="#">Logout</a>
+        </li>
+      </ul>
     </div>
   </header>
 </template>
 
 <script>
-export default {
-  name: "Header",
-};
+  export default {
+    name: "Header",
+  };
 </script>
 
 <style scoped>
 .header {
   padding: 0.5rem 0;
+  overflow: hidden;
+  /* max-height: 41px; */
 }
 .header-title {
   font-size: 1.3rem;
@@ -26,6 +39,17 @@ export default {
   position: absolute;
   right: 0;
   color: #333;
+}
+.navigation {
+}
+.navigation-item a {
+  display: block;
+  width: 100%;
+  height: 100%;
+  padding: 0.8rem;
+  font-size: 0.8rem;
+  color: #333;
+  font-weight: 600;
 }
 @media (min-width: 1024px) {
   .hb-btn {
